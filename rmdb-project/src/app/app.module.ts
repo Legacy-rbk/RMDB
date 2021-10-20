ab1b846adb772c463822b284c4e40267fc471ff1
ghofraneHammoudi <ga.frouna123@gmail.com>
2021-10-20 21:05:23 +0100

    the nav bar almost done

--- rmdb-project/src/app/app.module.ts
+++ rmdb-project/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstNavComponent } from './First-Nav/First-Nav.component';
 import { SliderShowComponent } from './Slider-Show/Slider-Show.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
+
 
 @NgModule({
   declarations: [		
@@ -14,7 +18,10 @@ import { SliderShowComponent } from './Slider-Show/Slider-Show.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
