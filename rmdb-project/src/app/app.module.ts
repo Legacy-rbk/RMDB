61c0c7a94e616c941ba1627065daae46a2371635
mnawa3 <fouratir16@gmail.com>
2021-10-20 20:27:28 +0100

    trying popup

--- rmdb-project/src/app/app.module.ts
+++ rmdb-project/src/app/app.module.ts
@@ -5,18 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstNavComponent } from './First-Nav/First-Nav.component';
 import { SliderShowComponent } from './Slider-Show/Slider-Show.component';
+import { AuthPopupComponent } from './auth-popup/auth-popup.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatDialogModule ,MAT_DIALOG_DEFAULT_OPTIONS} from '@angular/material/dialog';
+
+
 
 @NgModule({
-  declarations: [		
+  declarations: [			
     AppComponent,
       FirstNavComponent,
-      SliderShowComponent
+      SliderShowComponent,
+      AuthPopupComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [{provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}}],
   bootstrap: [AppComponent]
+ 
 })
 export class AppModule { }
