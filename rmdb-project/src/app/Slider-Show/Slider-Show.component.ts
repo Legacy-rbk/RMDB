e515f0a01c3412e099ad4867651b60b39fba534f
slimkhemiri <khemiri.medslim1@gmail.com>
2021-10-24 14:59:53 +0100

    hover with description for all cards

--- rmdb-project/src/app/Slider-Show/Slider-Show.component.ts
+++ rmdb-project/src/app/Slider-Show/Slider-Show.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit , Input } from '@angular/core';
 import {DataService} from '../data.service'
 import { Subscription } from 'rxjs';
-import {Movie} from '../Movie'
-
+import {Movie} from '../Movie';
 
 
 @Component({
@@ -10,25 +9,26 @@ import {Movie} from '../Movie'
   templateUrl: './Slider-Show.component.html',
   styleUrls: ['./Slider-Show.component.css'],
 })
+
+
 export class SliderShowComponent {
- 
-  getSliders:Subscription[]=[]
-  sliders:Movie
-  @Input() loggedin: boolean 
- watchList(){
- if(this.loggedin === false){
-   alert('you must login first !')
+
+getSliders:Subscription[]=[]
+sliders:Movie
+@Input() loggedin: boolean 
+watchList(){
+if(this.loggedin === false){
+alert('you must login first !')
  }else
   alert('added to watchlist')
-
 }
 
-  constructor( private  movie:DataService) { 
+constructor( private  movie:DataService) { 
    
-  }
-
-  ngOnInit():void {
-    this.getSliders.push(this.movie.getconfig1().subscribe(data=>this.sliders=data))
+}
+ngOnInit():void {
+this.getSliders.push(this.movie.getconfig1().subscribe(data=>this.sliders=data))
   }
 
 }
+ 
